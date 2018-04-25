546b09d4e79afe124ab5c5859a5a5cacb1a3cf50
bbak0 <b_bak@outlook.com>
2018-04-25 15:15:46 +0200

    skeleton code

--- src/com/packing/Main.java
+++ src/com/packing/Main.java
@@ -1,8 +1,28 @@
 package com.packing;
 
+import com.packing.algo.AbstractAlgorithm;
+import com.packing.algo.ExampleAlgo;
+import com.packing.algo.OtherAlgo;
+import com.packing.models.Data;
+
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        InputReader inputReader = new InputReader();
+        Data input = inputReader.read();
+        System.out.println(input);
+        AbstractAlgorithm solver;
+
+        Random random = new Random();
+
+        if (random.nextBoolean()){
+            solver = new ExampleAlgo(input);
+        } else {
+            solver = new OtherAlgo(input);
+        }
+
+        solver.solve();
     }
 }
