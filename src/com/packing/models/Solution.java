7bc36b0ca1f8e743b6e2f46404247c6c6729a1e9
Ameya Rao <ameyarao98@gmail.com>
2018-05-16 15:30:51 +0200

    made area in Rectangle an int type and added the AreaComparator

--- src/com/packing/models/Solution.java
+++ src/com/packing/models/Solution.java
@@ -5,15 +5,9 @@ public class Solution {
 
      public ArrayList<Rectangle> rectangles;
     public long area;
-    public long wastedArea;
     public int maxHeight = 0;
     public int maxWidth = 0;
 
-
-    public void update(){
-        calcDimensions();
-        calcWastedArea();
-    }
 
     public void calcDimensions(){
         for(Rectangle r: rectangles){
@@ -25,13 +19,5 @@ public class Solution {
             }
         }
         area = maxHeight*maxHeight;
-    }
-    public void calcWastedArea(){
-        long rectangleArea = 0;
-        for(Rectangle r: rectangles){
-            rectangleArea += r.area;
-        }
-        wastedArea = area - rectangleArea;
-
     }
 }
