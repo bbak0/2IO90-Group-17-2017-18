fdef38aad55a432c738f7f06f2af91e93ab7a903
Ameya Rao <ameyarao98@gmail.com>
2018-05-31 19:12:01 +0200

    Slight modifications to annealing + recursive heuristic

--- src/com/packing/algo/RecursiveHeuristic.java
+++ src/com/packing/algo/RecursiveHeuristic.java
@@ -1,8 +1,6 @@
 package com.packing.algo;
 
-import com.packing.models.Data;
-import com.packing.models.Rectangle;
-import com.packing.models.Solution;
+import com.packing.models.*;
 import com.packing.sorting.IndexComparator;
 import com.packing.sorting.WidthComparator;
 
@@ -15,43 +13,26 @@ public class RecursiveHeuristic extends AbstractAlgorithm {
         super(in);
     }
 
-    int currentLevelWidth = 0;
-    int currentFloorHeight = 0;
-    int containerHeight = input.getContainerHeight();
-    int formerFirstRectangleWidth = 0;
-    ArrayList<Rectangle> rectangleCollection = input.getRectangles();
-
     @Override
     public Solution solve() {
-        for (Rectangle rectangle : rectangleCollection) {
-            if (floor_feasible(rectangle) == false) {
-                createNewLevel(rectangle);
-            }
-            rectangle.placeRectangle(currentLevelWidth, currentFloorHeight);
-            currentFloorHeight = currentFloorHeight + rectangle.getHeight();  //(HAS to be after packing)
-        }
-        Collections.sort(rectangleCollection, new IndexComparator());
-        return new Solution(rectangleCollection);
+
+        return new Solution();
     }
 
+    void RecursivePacking(BoundedSpace bs){
+        if(true){
+            return;
+        }
 
 
-    boolean floor_feasible(Rectangle r){
-        if(currentFloorHeight == 0 && currentLevelWidth == 0){
-            formerFirstRectangleWidth = r.getWidth();               //initialising variable manually to largest width rectangle
-        }
-        if (r.getHeight() <= containerHeight - currentFloorHeight ){
-            return true;
-        }
-        else {
-            return false;
-        }
     }
 
-    void createNewLevel(Rectangle r) {
-        currentLevelWidth = currentLevelWidth + formerFirstRectangleWidth;
-        currentFloorHeight = 0;
-        formerFirstRectangleWidth = r.getWidth();
+    void Packing(UnboundedSpace us){
+
+    }
+
+    void HeuristicRecursion(){
+
     }
 
 
